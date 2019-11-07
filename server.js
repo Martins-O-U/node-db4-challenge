f4b09861f5d3204a49dd17fa7f0f2e58b0944c09
Martins-O-U <martinsonyedikachi@gmail.com>
2019-11-07 15:45:22 +0100

    Migrations ran and database tables created for recipes, ingredients etc

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const helmet = require('helmet');
 
-const db = require('./data/db-config.js');
+// const db = require('./data/db-config.js');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+
+server.get('/', (req, res) =>{
+    res.json('This is the defauls zone')
+})
+
+module.exports = server;
